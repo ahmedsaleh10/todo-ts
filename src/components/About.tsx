577abd0a4c3b5f1bee4899861be0302070c38543
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-10-22 20:01:31 +0300

    adding the about component.

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <h3>I hope you happy to show my work.</h3>  
+      <a href='/' >Go Back</a>
+    </>
+    
+  )
+}
+
+export default About 
